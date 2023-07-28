fe8c8619381f5fa8d4a187d04897a0c67a78c1db
iamsumansaha <suman.saha5858@.com>
2023-07-28 23:04:41 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
-import Companies from "./components/companies/companies";
+import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
-import Value from "./components/Value/value";
+import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
 import GetStarted from "./components/GetStarted/GetStarted";
 import Footer from "./components/Footer/Footer";
